868507221d76f42d09447876644399221739b2a1
LusseveldNiek <97088465@st.deltion.nl>
2023-05-08 16:04:18 +0200

    movement

--- Bruin Hol/Assets/Scripts/MovementPlayer.cs
+++ Bruin Hol/Assets/Scripts/MovementPlayer.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class MovementPlayer : MonoBehaviour
 {
     public float speedPlayer;
-    public Rigidbody player;
+    public float jumpForce;
+    public float feetRadius;
+    public Rigidbody playerRigidbody;
+
+    public bool isGrounded;
+    public Transform feet;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +20,28 @@ public class MovementPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        player.AddForce(Input.GetAxis("Horizontal") * speedPlayer, 0, 0);
-        player.AddForce(0, Input.GetAxis("Vertical") * speedPlayer, 0);
+
+        playerRigidbody.AddForce(Input.GetAxis("Horizontal") * speedPlayer, 0, 0);
+
+        if(Input.GetKey("w") && isGrounded)
+        {
+            playerRigidbody.AddForce(Vector3.up * jumpForce);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
     }
 }
