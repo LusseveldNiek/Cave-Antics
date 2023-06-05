db67155935402109b73960bf7426b0749368be5e
Weezle0 <wesselbrattinga@gmail.com>
2023-06-05 15:16:52 +0200

    Load/Unload Area

---
+++ Bruin Hol/Assets/Scripts/LoadUnloadArea.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadUnloadArea : MonoBehaviour
+{
+    public GameObject areaToDisable;
+    public GameObject areaToEnable;
+    bool canToggle = true;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.gameObject.tag == "Player")
+        {
+            if (canToggle)
+            {
+                canToggle = false;
+                StartCoroutine(WaitForFix());
+                if (areaToDisable.activeInHierarchy)
+                {
+                    areaToDisable.SetActive(false);
+                    areaToEnable.SetActive(true);
+                }
+                else
+                {
+                    areaToDisable.SetActive(true);
+                    areaToEnable.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public IEnumerator WaitForFix()
+    {
+        yield return new WaitForSeconds(0.5f);
+        canToggle = true;
+    }
+}
