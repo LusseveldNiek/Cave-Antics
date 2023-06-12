569065f31bbf8a29bcb53681465e3c2d2ab1edc2
LusseveldNiek <97088465@st.deltion.nl>
2023-06-12 15:09:17 +0200

    bullet rotation

--- Bruin Hol/Assets/Scripts/FlowerBullet.cs
+++ Bruin Hol/Assets/Scripts/FlowerBullet.cs
@@ -5,7 +5,18 @@ using UnityEngine;
 public class FlowerBullet : MonoBehaviour
 {
     public float speed;
+    public float rotationSpeed;
+    public Rigidbody rb;
 
+    public void Update()
+    {
+        if (rb.velocity.magnitude > 0.1f) // Check if the object is moving
+        {
+            Vector3 direction = rb.velocity.normalized;
+            Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        }
+    }
     // Shoot the bullet in the specified direction
     public void ShootInDirection(Vector3 direction)
     {
