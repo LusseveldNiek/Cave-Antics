3c19778e6e5d5a57cea54fff1535d646a1397ac7
LusseveldNiek <97088465@st.deltion.nl>
2023-06-20 16:19:29 +0200

    enemys

--- Bruin Hol/Assets/Scripts/VierkantSteen.cs
+++ Bruin Hol/Assets/Scripts/VierkantSteen.cs
@@ -4,58 +4,17 @@ using UnityEngine;
 
 public class VierkantSteen : MonoBehaviour
 {
-    public GameObject cube;
-    public Transform rotations;
-
-    public Transform downRight;
-    public Transform downLeft;
-    public Transform upRight;
-    public Transform upLeft;
+    public float animationTime;
+    private float animationTimer;
 
     void Update()
     {
-        if(cube.transform.rotation.z == 0)
+        animationTimer += Time.deltaTime;
+        if(animationTimer > animationTime)
         {
-            //downright
-            downLeft.SetParent(rotations);
-            downLeft.rotation = Quaternion.identity;
-
-            cube.transform.SetParent(downRight);
-            downRight.Rotate(0, 0, 90); 
+            transform.position -= new Vector3(8.3f, 0, 0);
+            animationTimer = 0;
         }
-
-        if(cube.transform.rotation.z == 90)
-        {
-            //upright
-            downRight.SetParent(rotations);
-            downRight.rotation = Quaternion.identity;
-
-            cube.transform.SetParent(upRight);
-            upRight.Rotate(0, 0, 90);
-            
-        }
-
-        if (cube.transform.rotation.z == 180)
-        {
-            //upleft
-            upRight.SetParent(rotations);
-            upRight.rotation = Quaternion.identity;
-
-            cube.transform.SetParent(upLeft);
-            upLeft.Rotate(0, 0, 90);
-            
-        }
-
-        if (cube.transform.rotation.z == 270)
-        {
-            //downleft
-            upLeft.SetParent(rotations);
-            upLeft.rotation = Quaternion.identity;
-
-            cube.transform.SetParent(downLeft);
-            downLeft.Rotate(0, 0, 90);
-        }
-
     }
 
     
