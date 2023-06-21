fd50dad6817be8f8368c69f42b8913d972b7c113
LusseveldNiek <97088465@st.deltion.nl>
2023-06-21 11:43:29 +0200

    enemys

--- Bruin Hol/Assets/Scripts/VierkantSteen.cs
+++ Bruin Hol/Assets/Scripts/VierkantSteen.cs
@@ -4,18 +4,82 @@ using UnityEngine;
 
 public class VierkantSteen : MonoBehaviour
 {
-    public float animationTime;
-    private float animationTimer;
+    public bool goingLeft;
+    public bool goingRight;
+
+    public Transform raycastUp;
+    public Transform raycastDown;
+    public Transform raycastRight;
+    public Transform raycastLeft;
+
+    private RaycastHit hit;
+    public Animator animator;
 
     void Update()
     {
-        animationTimer += Time.deltaTime;
-        if(animationTimer > animationTime)
+        if(goingLeft)
         {
-            transform.position -= new Vector3(8.3f, 0, 0);
-            animationTimer = 0;
+            
+            Physics.Raycast(raycastUp.position, Vector3.left, out hit, 0.1f);
+            Physics.Raycast(raycastDown.position, Vector3.left, out hit, 0.1f);
+            Physics.Raycast(raycastLeft.position, Vector3.left, out hit, 0.1f);
+            Physics.Raycast(raycastRight.position, Vector3.left, out hit, 0.1f);
+
+            if(hit.transform != null)
+            {
+                if (hit.transform.gameObject.tag == "wallLeft")
+                {
+                    animator.SetFloat("speed", -1);
+                    goingRight = true;
+                    goingLeft = false;
+                    hit = new RaycastHit();
+                    print("hitWall");
+                }
+            }
+            
+        }
+
+        if(goingRight)
+        {
+            
+            Physics.Raycast(raycastUp.position, Vector3.right, out hit, 0.1f);
+            Physics.Raycast(raycastDown.position, Vector3.right, out hit, 0.1f);
+            Physics.Raycast(raycastLeft.position, Vector3.right, out hit, 0.1f);
+            Physics.Raycast(raycastRight.position, Vector3.right, out hit, 0.1f);
+
+            if(hit.transform != null)
+            {
+                if (hit.transform.gameObject.tag == "wallRight")
+                {
+                    animator.SetFloat("speed", 1);
+                    goingRight = false;
+                    goingLeft = true;
+                    hit = new RaycastHit();
+                }
+            }
+            
+        }
+        
+    }
+
+    public void Leftreset()
+    {
+        if(goingLeft)
+        {
+            transform.position += new Vector3(-9.288f, 0, 0);
+            print("teleportingLeft");
+        }
+    }
+
+    public void Rightreset()
+    {
+        if (goingRight)
+        {
+            transform.position += new Vector3(9.288f, 0, 0);
+            print("teleportingRight");
         }
     }
 
     
+
 }
