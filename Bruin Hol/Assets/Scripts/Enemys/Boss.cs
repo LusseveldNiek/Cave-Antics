cb6b7c9c20ee196848354a413a22a9acab79638c
LusseveldNiek <97088465@st.deltion.nl>
2023-07-05 15:07:24 +0200

    bugs

---
+++ Bruin Hol/Assets/Scripts/Enemys/Boss.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+    public Animator animator;
+    public GameObject particle;
+    private bool attack1;
+    private bool attack2;
+    private bool reset;
+    private float resetTime;
+    public float fireSpeed;
+
+    public GameObject prefabAttack1;
+    public GameObject prefabAttack2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        reset = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(reset)
+        {
+            resetTime += Time.deltaTime;
+            if(resetTime > 2)
+            {
+                attack1 = true;
+                resetTime = 0;
+                reset = false;
+            }
+        }
+
+        if(attack1)
+        {
+            animator.SetBool("attack1", true);
+
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "pickaxe")
+        {
+            Destroy(gameObject);
+            GameObject particlePrefab = Instantiate(particle, transform.position, Quaternion.identity);
+            Destroy(particlePrefab, 1);
+        }
+    }
+
+    public void Schoot()
+    {
+        GameObject prefab = Instantiate(prefabAttack1, transform.position, Quaternion.identity);
+        prefab.transform.Translate(Vector3.right * fireSpeed);
+    }
+}
